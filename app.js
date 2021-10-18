6a18b5c8ea9644a1adc44513fa0621ac0c7333fc
parthpanchal123 <parthpanchal53@gmail.com>
2021-10-18 18:26:24 +0530

    Added support for custom links

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // Button Refernces
 
+// For add new field btn
+const addNewBtn = document.getElementById("addNewBtn");
+
+// The div that contains all the fields
+
+const linksDiv = document.querySelector(".links-area");
+
 // For Github
 const gitEditBtn = document.getElementById("gitEditBtn");
 const gitLink = document.getElementById("Github");
@@ -133,6 +140,13 @@ const showalert = (linkData) => {
 };
 
 const loadAllFromLocalStorage = () => {
+  // An array for custom fields
+  let customFields = JSON.parse(localStorage.getItem("customFields"));
+
+  customFields.forEach((field) => {
+    loadCustomFieldsFromLocalStorage(field);
+  });
+
   const gitLinkData = localStorage.getItem("Github");
   const linkedInLinkData = localStorage.getItem("Linkedin");
   const twitterLinkData = localStorage.getItem("Twitter");
@@ -148,6 +162,202 @@ const loadAllFromLocalStorage = () => {
   emailLink.value = emailLinkData;
   devLink.value = devLinkData;
   dribbbleLink.value = dribbbleLinkData;
+};
+
+const loadCustomFieldsFromLocalStorage = (newFieldName) => {
+  const newField = document.createElement("div");
+  newField.classList.add(
+    "flex",
+    "flex-row",
+    "justify-center",
+    "items-center",
+    "ml-3"
+  );
+
+  const newIcon = document.createElement("i");
+  newIcon.style.fontSize = "28px";
+  newIcon.classList.add("fas", "fa-link");
+
+  const newEditIcon = document.createElement("i");
+  newEditIcon.classList.add("fas", "fa-edit");
+
+  const newCopyIcon = document.createElement("i");
+  newCopyIcon.classList.add("fas", "fa-copy");
+
+  const newFieldInput = document.createElement("input");
+  newFieldInput.classList.add(
+    "w-3/5",
+    "ml-4",
+    "px-2",
+    "py-2",
+    "shadow-md",
+    "rounded-md",
+    "text-left"
+  );
+
+  newFieldInput.setAttribute("placeholder", newFieldName);
+  newFieldInput.setAttribute("id", newFieldName);
+  newFieldInput.value = localStorage.getItem(newFieldName);
+  newFieldInput.setAttribute("type", "text");
+
+  const newFieldEditBtn = document.createElement("button");
+  newFieldEditBtn.classList.add(
+    "m-2",
+    "bg-blue-500",
+    "hover:bg-blue-700",
+    "text-white",
+    "font-bold",
+    "py-2",
+    "px-4",
+    "rounded-md",
+    "shadow-md"
+  );
+  newFieldEditBtn.setAttribute("id", newFieldName + "EditBtn");
+
+  newFieldEditBtn.appendChild(newEditIcon);
+
+  newFieldEditBtn.addEventListener("click", () => {
+    const data = document.getElementById(newFieldName).value;
+    saveToLocalStorage(newFieldName, data);
+  });
+
+  const newFieldCopyBtn = document.createElement("button");
+  newFieldCopyBtn.classList.add(
+    "m-2",
+    "ml-2",
+    "bg-blue-500",
+    "hover:bg-blue-700",
+    "text-white",
+    "font-bold",
+    "py-2",
+    "px-4",
+    "rounded-md",
+    "shadow-md"
+  );
+
+  newFieldCopyBtn.setAttribute("id", newFieldName + "CopyBtn");
+  newFieldCopyBtn.appendChild(newCopyIcon);
+
+  newFieldCopyBtn.addEventListener("click", () => {
+    const data = document.getElementById(newFieldName).value;
+    copyToClipboard(data);
+  });
+
+  newField.appendChild(newIcon);
+  newField.appendChild(newFieldInput);
+  newField.appendChild(newFieldEditBtn);
+  newField.appendChild(newFieldCopyBtn);
+
+  linksDiv.appendChild(newField);
+};
+
+// Add a new field
+
+const addNewField = () => {
+  const newFieldName = prompt("Enter a new field name");
+  if (!newFieldName) return;
+
+  if (checkForDuplicates(newFieldName)) return;
+
+  const newField = document.createElement("div");
+  newField.classList.add("flex", "flex-row", "justify-center", "items-center");
+
+  const newIcon = document.createElement("i");
+  newIcon.style.fontSize = "28px";
+  newIcon.classList.add("fas", "fa-link");
+
+  const newEditIcon = document.createElement("i");
+  newEditIcon.classList.add("fas", "fa-edit");
+
+  const newCopyIcon = document.createElement("i");
+  newCopyIcon.classList.add("fas", "fa-copy");
+
+  const newFieldInput = document.createElement("input");
+  newFieldInput.classList.add(
+    "w-3/5",
+    "ml-4",
+    "px-2",
+    "py-2",
+    "shadow-md",
+    "rounded-md",
+    "text-left"
+  );
+
+  newFieldInput.setAttribute("placeholder", newFieldName);
+  newFieldInput.setAttribute("id", newFieldName);
+  newFieldInput.setAttribute("type", "text");
+
+  const newFieldEditBtn = document.createElement("button");
+  newFieldEditBtn.classList.add(
+    "m-2",
+    "bg-blue-500",
+    "hover:bg-blue-700",
+    "text-white",
+    "font-bold",
+    "py-2",
+    "px-4",
+    "rounded-md",
+    "shadow-md"
+  );
+  newFieldEditBtn.setAttribute("id", newFieldName + "EditBtn");
+
+  newFieldEditBtn.appendChild(newEditIcon);
+
+  newFieldEditBtn.addEventListener("click", () => {
+    const data = document.getElementById(newFieldName).value;
+    saveToLocalStorage(newFieldName, data);
+  });
+
+  const newFieldCopyBtn = document.createElement("button");
+  newFieldCopyBtn.classList.add(
+    "bg-blue-500",
+    "hover:bg-blue-700",
+    "text-white",
+    "font-bold",
+    "py-2",
+    "px-4",
+    "rounded-md",
+    "shadow-md"
+  );
+
+  newFieldCopyBtn.setAttribute("id", newFieldName + "CopyBtn");
+  newFieldCopyBtn.appendChild(newCopyIcon);
+
+  newFieldCopyBtn.addEventListener("click", () => {
+    const data = document.getElementById(newFieldName).value;
+    copyToClipboard(data);
+  });
+
+  newField.appendChild(newIcon);
+  newField.appendChild(newFieldInput);
+  newField.appendChild(newFieldEditBtn);
+  newField.appendChild(newFieldCopyBtn);
+
+  linksDiv.appendChild(newField);
+
+  const newItems = JSON.parse(localStorage.getItem("customFields")) ?? [];
+  newItems.push(newFieldName);
+
+  console.log(newItems);
+
+  localStorage.setItem("customFields", JSON.stringify(newItems));
+
+  // Get the field name
+};
+
+addNewBtn.addEventListener("click", () => {
+  addNewField();
+});
+
+const checkForDuplicates = (newLinkName) => {
+  const links = JSON.parse(localStorage.getItem("customFields"));
+
+  if (!links) return;
+
+  if (links.includes(newLinkName)) {
+    alert("This field already exists");
+    return true;
+  }
 };
 
 // Load values from local storage
