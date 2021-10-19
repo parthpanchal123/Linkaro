feadad272271fdd679621db0c638951daa514b4e
parthpanchal123 <parthpanchal53@gmail.com>
2021-10-19 09:48:44 +0530

    Refactored code

--- app.js
+++ app.js
@@ -1,127 +1,21 @@
-// Button Refernces
-
-// For add new field btn
+// Button Refernce for add new field btn
 const addNewBtn = document.getElementById("addNewBtn");
 
 // The div that contains all the fields
-
 const linksDiv = document.querySelector(".links-area");
 
-// For Github
-const gitEditBtn = document.getElementById("gitEditBtn");
-const gitLink = document.getElementById("Github");
-const gitCopyBtn = document.getElementById("gitCopyBtn");
-
-// For linkedin
-const linkedinEditBtn = document.getElementById("linkedInEditBtn");
-const linkedInLink = document.getElementById("Linkedin");
-const linkedinCopyBtn = document.getElementById("linkedInCopyBtn");
-
-// For twitter
-
-const twitterEditBtn = document.getElementById("twitterEditBtn");
-const twitterLink = document.getElementById("Twitter");
-const twitterCopyBtn = document.getElementById("twitterCopyBtn");
-
-// For Portfolio
-
-const portfolioEditBtn = document.getElementById("portfolioEditBtn");
-const portfolioLink = document.getElementById("Portfolio");
-const portfolioCopyBtn = document.getElementById("portfolioCopyBtn");
-
-// For Email
-
-const emailEditBtn = document.getElementById("emailEditBtn");
-const emailLink = document.getElementById("Email");
-const emailCopyBtn = document.getElementById("emailCopyBtn");
-
-// For Dev.to
-
-const devEditBtn = document.getElementById("devEditBtn");
-const devLink = document.getElementById("Dev");
-const devCopyBtn = document.getElementById("devCopyBtn");
-
-// For Dribbble
-
-const dribbbleEditBtn = document.getElementById("dribbbleEditBtn");
-const dribbbleLink = document.getElementById("Dribbble");
-const dribbbleCopyBtn = document.getElementById("dribbbleCopyBtn");
-
-gitEditBtn.addEventListener("click", () => {
-  const gitLinkUrl = gitLink.value;
-  saveToLocalStorage("Github", gitLinkUrl);
-});
-
-gitCopyBtn.addEventListener("click", (event) => {
-  const gitLinkUrl = gitLink.value;
-  copyToClipboard(gitLinkUrl);
-});
-
-linkedInEditBtn.addEventListener("click", () => {
-  const linkedInLinkUrl = linkedInLink.value;
-  saveToLocalStorage("Linkedin", linkedInLinkUrl);
-});
-
-linkedInCopyBtn.addEventListener("click", (event) => {
-  const linkedInLinkUrl = gitLink.value;
-  copyToClipboard(linkedInLinkUrl);
-});
-
-twitterEditBtn.addEventListener("click", () => {
-  const twitterLinkUrl = twitterLink.value;
-  saveToLocalStorage("Twitter", twitterLinkUrl);
-});
-
-twitterCopyBtn.addEventListener("click", (event) => {
-  const twitterLinkUrl = twitterLink.value;
-  copyToClipboard(twitterLinkUrl);
-});
-
-portfolioEditBtn.addEventListener("click", () => {
-  const portfolioLinkUrl = portfolioLink.value;
-  saveToLocalStorage("Portfolio", portfolioLinkUrl);
-});
-
-portfolioCopyBtn.addEventListener("click", (event) => {
-  const portfolioLinkUrl = portfolioLink.value;
-  copyToClipboard(portfolioLinkUrl);
-});
-
-emailEditBtn.addEventListener("click", () => {
-  const emailLinkUrl = emailLink.value;
-  saveToLocalStorage("Email", emailLinkUrl);
-});
-
-emailCopyBtn.addEventListener("click", (event) => {
-  const emailLinkUrl = emailLink.value;
-  copyToClipboard(emailLinkUrl);
-});
-
-devEditBtn.addEventListener("click", () => {
-  const devLinkUrl = devLink.value;
-  saveToLocalStorage("Dev", devLinkUrl);
-});
-
-devCopyBtn.addEventListener("click", (event) => {
-  const devLinkUrl = devLink.value;
-  copyToClipboard(devLinkUrl);
-});
-
-dribbbleEditBtn.addEventListener("click", () => {
-  const dribbbleLinkUrl = dribbbleLink.value;
-  saveToLocalStorage("Dribbble", dribbbleLinkUrl);
-});
-
-dribbbleCopyBtn.addEventListener("click", (event) => {
-  const dribbbleLinkUrl = dribbbleLink.value;
-  copyToClipboard(dribbbleLinkUrl);
-});
-
+/**
+ * @param  {String} key The name of the field
+ * @param  {String} linkData The data of the field
+ */
 const saveToLocalStorage = (key, linkData) => {
   if (!linkData) return;
   localStorage.setItem(key, linkData);
 };
 
+/**
+ * @param  {String} urlData The linkdata of the field
+ */
 const copyToClipboard = async (urlData) => {
   try {
     await navigator.clipboard.writeText(urlData);
@@ -131,6 +25,9 @@ const copyToClipboard = async (urlData) => {
   }
 };
 
+/**
+ * @param  {String} linkData The data of the field
+ */
 const showalert = (linkData) => {
   if (!linkData) {
     alert("No link to copy ❌ ");
@@ -140,33 +37,31 @@ const showalert = (linkData) => {
 };
 
 const loadAllFromLocalStorage = () => {
+  const defaultFields = [
+    "Github",
+    "Linkedin",
+    "Twitter",
+    "Portfolio",
+    "Email",
+    "Dev",
+    "Dribbble",
+  ];
   // An array for custom fields
   let customFields = JSON.parse(localStorage.getItem("customFields")) ?? [];
 
   if (customFields.length != 0) {
     customFields.forEach((field) => {
-      loadCustomFieldsFromLocalStorage(field);
+      renderFieldsFromLocalStorage({ newFieldName: field, isNewField: false });
     });
+  } else {
+    localStorage.setItem("customFields", JSON.stringify(defaultFields));
   }
-
-  const gitLinkData = localStorage.getItem("Github");
-  const linkedInLinkData = localStorage.getItem("Linkedin");
-  const twitterLinkData = localStorage.getItem("Twitter");
-  const portfolioLinkData = localStorage.getItem("Portfolio");
-  const emailLinkData = localStorage.getItem("Email");
-  const devLinkData = localStorage.getItem("Dev");
-  const dribbbleLinkData = localStorage.getItem("Dribbble");
-
-  gitLink.value = gitLinkData;
-  linkedInLink.value = linkedInLinkData;
-  twitterLink.value = twitterLinkData;
-  portfolioLink.value = portfolioLinkData;
-  emailLink.value = emailLinkData;
-  devLink.value = devLinkData;
-  dribbbleLink.value = dribbbleLinkData;
 };
-
-const loadCustomFieldsFromLocalStorage = (newFieldName) => {
+/**
+ * @param  {String} newFieldName The name of the field
+ * @param  {Boolean} isNewField A boolean to check if the field is new or not
+ */
+const renderFieldsFromLocalStorage = ({ newFieldName, isNewField }) => {
   const newField = document.createElement("div");
   newField.classList.add(
     "flex",
@@ -178,7 +73,33 @@ const loadCustomFieldsFromLocalStorage = (newFieldName) => {
 
   const newIcon = document.createElement("i");
   newIcon.style.fontSize = "28px";
-  newIcon.classList.add("fas", "fa-link");
+
+  switch (newFieldName) {
+    case "Github":
+      newIcon.classList.add("fab", "fa-github");
+      break;
+    case "Linkedin":
+      newIcon.classList.add("fab", "fa-linkedin");
+      break;
+    case "Twitter":
+      newIcon.classList.add("fab", "fa-twitter");
+      break;
+    case "Portfolio":
+      newIcon.classList.add("fas", "fa-globe");
+      break;
+    case "Email":
+      newIcon.classList.add("fas", "fa-envelope");
+      break;
+    case "Dev":
+      newIcon.classList.add("fab", "fa-dev");
+      break;
+    case "Dribbble":
+      newIcon.classList.add("fab", "fa-dribbble");
+      break;
+    default:
+      newIcon.classList.add("fas", "fa-link");
+      break;
+  }
 
   const newEditIcon = document.createElement("i");
   newEditIcon.classList.add("fas", "fa-edit");
@@ -225,93 +146,6 @@ const loadCustomFieldsFromLocalStorage = (newFieldName) => {
 
   const newFieldCopyBtn = document.createElement("button");
   newFieldCopyBtn.classList.add(
-    "m-2",
-    "ml-2",
-    "bg-blue-500",
-    "hover:bg-blue-700",
-    "text-white",
-    "font-bold",
-    "py-2",
-    "px-4",
-    "rounded-md",
-    "shadow-md"
-  );
-
-  newFieldCopyBtn.setAttribute("id", newFieldName + "CopyBtn");
-  newFieldCopyBtn.appendChild(newCopyIcon);
-
-  newFieldCopyBtn.addEventListener("click", () => {
-    const data = document.getElementById(newFieldName).value;
-    copyToClipboard(data);
-  });
-
-  newField.appendChild(newIcon);
-  newField.appendChild(newFieldInput);
-  newField.appendChild(newFieldEditBtn);
-  newField.appendChild(newFieldCopyBtn);
-
-  linksDiv.appendChild(newField);
-};
-
-// Add a new field
-
-const addNewField = () => {
-  const newFieldName = prompt("Enter a new field name");
-  if (!newFieldName) return;
-
-  if (checkForDuplicates(newFieldName)) return;
-
-  const newField = document.createElement("div");
-  newField.classList.add("flex", "flex-row", "justify-center", "items-center");
-
-  const newIcon = document.createElement("i");
-  newIcon.style.fontSize = "28px";
-  newIcon.classList.add("fas", "fa-link");
-
-  const newEditIcon = document.createElement("i");
-  newEditIcon.classList.add("fas", "fa-edit");
-
-  const newCopyIcon = document.createElement("i");
-  newCopyIcon.classList.add("fas", "fa-copy");
-
-  const newFieldInput = document.createElement("input");
-  newFieldInput.classList.add(
-    "w-3/5",
-    "ml-4",
-    "px-2",
-    "py-2",
-    "shadow-md",
-    "rounded-md",
-    "text-left"
-  );
-
-  newFieldInput.setAttribute("placeholder", newFieldName);
-  newFieldInput.setAttribute("id", newFieldName);
-  newFieldInput.setAttribute("type", "text");
-
-  const newFieldEditBtn = document.createElement("button");
-  newFieldEditBtn.classList.add(
-    "m-2",
-    "bg-blue-500",
-    "hover:bg-blue-700",
-    "text-white",
-    "font-bold",
-    "py-2",
-    "px-4",
-    "rounded-md",
-    "shadow-md"
-  );
-  newFieldEditBtn.setAttribute("id", newFieldName + "EditBtn");
-
-  newFieldEditBtn.appendChild(newEditIcon);
-
-  newFieldEditBtn.addEventListener("click", () => {
-    const data = document.getElementById(newFieldName).value;
-    saveToLocalStorage(newFieldName, data);
-  });
-
-  const newFieldCopyBtn = document.createElement("button");
-  newFieldCopyBtn.classList.add(
     "bg-blue-500",
     "hover:bg-blue-700",
     "text-white",
@@ -337,21 +171,24 @@ const addNewField = () => {
 
   linksDiv.appendChild(newField);
 
-  const newItems = JSON.parse(localStorage.getItem("customFields")) ?? [];
-  newItems.push(newFieldName);
-
-  console.log(newItems);
-
-  localStorage.setItem("customFields", JSON.stringify(newItems));
-
-  // Get the field name
+  if (isNewField) {
+    let existingFields = JSON.parse(localStorage.getItem("customFields"));
+    existingFields.push(newFieldName);
+    localStorage.setItem("customFields", JSON.stringify(existingFields));
+  }
 };
 
 addNewBtn.addEventListener("click", () => {
-  addNewField();
-});
+  newFieldName = prompt("Enter a new field name");
+  if (!newFieldName) return;
 
-const checkForDuplicates = (newLinkName) => {
+  if (isDuplicateField(newFieldName)) return;
+  renderFieldsFromLocalStorage({ newFieldName, isNewField: true });
+});
+/**
+ * @param  {String} newLinkName The name of the field
+ */
+const isDuplicateField = (newLinkName) => {
   const links = JSON.parse(localStorage.getItem("customFields"));
 
   if (!links) return;
@@ -362,6 +199,5 @@ const checkForDuplicates = (newLinkName) => {
   }
 };
 
-// Load values from local storage
-
+// Load all the fields from local storage on startup.
 document.onload = loadAllFromLocalStorage();
